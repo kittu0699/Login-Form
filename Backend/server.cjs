d296be92eee5f703c8d9a4b597992e3305eabcd4
kittu0699 <kishanmishra6@gmail.com>
2025-08-25 17:57:01 +0530

    Done!

--- Backend/server.cjs
+++ Backend/server.cjs
@@ -10,17 +10,11 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const connection = mysql2.createConnection({
-  // host: "127.0.0.1",
-  // user: "root",
-  // password: "Kis766626@",
-  // port: 3306,
-  // database: "login",
-
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT, // usually 3306
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: "127.0.0.1",
+  user: "root",
+  password: "Kis766626@",
+  port: 3306,
+  database: "login",
 });
 
 connection.connect((err) => {
